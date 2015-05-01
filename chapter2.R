7f37cb2a3b11ea14cef7b4aeb1e871d2dc47da50
francoisgirouard <francoisgirouard@videotron.ca>
2015-05-01 06:48:10 -0400

    Chapter 2

--- chapter2.R
+++ chapter2.R
@@ -66,7 +66,51 @@ ApproximatePatternMatching <- function(Pattern, Text, d) {
 }
 
 
-fileName <- 'dataset_9_4.txt'
-Text <- readChar(fileName, file.info(fileName)$size)
-result <- ApproximatePatternMatching('TGAAACGGAGA', Text, 6)
-write(result, ncolumns = length(result))
+# fileName <- 'dataset_9_4.txt'
+# Text <- readChar(fileName, file.info(fileName)$size)
+# result <- ApproximatePatternMatching('TGAAACGGAGA', Text, 6)
+# write(result, ncolumns = length(result))
+ApproximatePatternCount <- function(Text, Pattern, d = 0) {
+  if (d == 0) {
+    return(PatternCount(Text, Pattern))
+  }
+  
+  count <- 0
+  
+  for (i in 1:(nchar(Text) - nchar(Pattern))) {
+    if (HammingDistance(Pattern,
+                        substring(Text, i, i + nchar(Pattern) - 1)) <= d) {
+      count <- count + 1
+    }
+  }
+  
+  return(count)
+}
+
+ApproximatePatternCount('AACAAGCTGATAAACATTTAAAGAG', 'AAAAA', 2)
+ApproximatePatternCount('TTTAGAGCCTTCAGAGG', 'GAGG', 2)
+
+fileConn <- file('dataset_9_6.txt', 'rc')
+Text <- trimws(readLines(fileConn, 1))
+Pattern <- trimws(readLines(fileConn, 1))
+d <- as.integer(trimws(readLines(fileConn, 1)))
+close(fileConn)
+ApproximatePatternCount(Text, Pattern, d)
+
+# Frequent Words with Mismatches Problem: Find the most frequent k-mers with mismatches in a string.
+# Input: A string Text as well as integers k and d. (You may assume k ≤ 12 and d ≤ 3.)
+# Output: All most frequent k-mers with up to d mismatches in Text.
+FrequentWordsWithMismatches <- function(Text, k, d) {
+    
+}
+
+k <- 12
+possibles <- character(4^k)
+for (i in k:1) {
+  possibles <- cbind(possibles, rep(c('A', 'C', 'G', 'T'),
+                                    each = 4^(k-(k-i+1)),
+                                    times = 4^(k-i)))
+}
+rep(c('A', 'C', 'G', 'T'), each = 4^(k-1), times = 4^(k-3))
+rep(c('A', 'C', 'G', 'T'), each = 4^(k-2), times = 4^(3-2))
+rep(c('A', 'C', 'G', 'T'), each = 4^(k-3), times = 4^(3-1))
